00f2a2ffbded6a9a0f5a07d8e457dfd9b6e6b540
Niki Catapano <nsc9605@gmail.com>
2020-11-21 15:06:53 -0500

    draft planner

---
+++ script-draft.js
@@ -0,0 +1,107 @@
+// Declaration of elements/classes
+const rows = $(".row");
+const hours = $(".hour");
+const past = $(".past");
+const present = $(".present");
+const future = $(".future");
+const timeBlock = $(".time-block");
+const saveButton = $(".saveBtn");
+const description = $(".description");
+const hourTask = $("id");
+const userText = $(".hour-task-text");
+
+
+// Set current time in header when application is opened
+$(document).ready(function () {
+  var today = moment();
+  $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
+
+  var hours = today.hours();
+  console.log(hours);
+});
+
+var parent = $(this).parents(".row");
+var timeId = parseInt(parent.attr("id"));
+var currentHour = parseInt(moment().format("H"));
+
+// set variable for current hour for color coding purposes
+let index = 0;
+
+console.log(timeBlock);
+
+timeBlock.each(function () {
+
+    var parent = $(this).parents(".row");
+    var timeId = parseInt(parent.attr("id"));
+    var currentHour = parseInt(moment().format("H"));
+    console.log(timeId);
+
+    if (timeId < currentHour) {
+      $(this).addClass("past");
+
+    } else if (timeId === currentHour) {
+        $(this).removeClass("past");
+      $(this).addClass("present");
+      
+    } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+    $(this).addClass("future");
+
+    $(this).val(localStorage.getItem(parent.attr("id")));
+  }}
+);
+
+
+
+
+function saveBtn() {
+  console.log($(this));
+
+
+$(".saveBtn").on("click", function() {
+    // $(".save").on("click", saveBtn)
+    // the save button we are clicking on
+    //  console.log($(this))
+   
+    var userText = $(this).siblings(".time-block").val();
+    console.log(userText);
+
+    // if(!localStorage.getItem(userText)) {
+    //    localStorage.setItem(userText, JSON.stringify(userText));
+    // }
+    // use jQuery to find this is hour
+    var hourTask = $(this).parents(".row").attr("id");
+    //is this value correct?
+    console.log(hourTask)
+
+    // use jQuery to find this is text content
+    // <- need to get the text content from here
+    
+// don't do this yet until you know we're doing the above correctly
+// localStorage.setItem(timeId);
+localStorage.setItem(hourTask, userText);
+// localStorage.setItem(hourTask,);
+// send it to localStorage
+// localStorage.setItem(hourTask, text)
+   
+})
+}
+// function hourTask(text) {
+
+// }
+// function saveBtn(event) {
+//     console.log(event.target);
+//     localStorage.setItem("text", text);
+// }
+// renderLastRegistered to keep information
+// function renderTasks ()
+
+
+// save to local storage
+
+
+// $("button").on("click", save);
+// function save() {
+// console.log($(this).siblings(textarea).val());
+// localStorage.setItem("text", JSON.stringify(text))}
